efb031ab520b73ca8c4b9f30c2d6d023949b68a4
abhijeetg10 <argaikwad24@gmail.com>
2026-09-23 22:28:27 +0530

    Fix pdf-parse crashing on Vercel by adding to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   // Allow cross-origin dev requests
   // @ts-ignore
   allowedDevOrigins: ['localhost', '127.0.0.1', '172.20.10.5'],
+  serverExternalPackages: ['pdf-parse'],
 };
 
 export default nextConfig;
